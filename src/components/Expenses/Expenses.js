6d477db3380daa2d78c869ba9468bdc055bdf989
Emir Emre Basdag <emrebasdag9@gmail.com>
2023-03-18 15:48:22 +0100

    react practices

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
+
+const Expenses = props => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <>
+      <Card className='expenses'>
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {/* {filteredExpenses.length === 0 ? (
+          <p>No expenses found.</p>
+        ) : (
+          filteredExpenses.map(expenses => (
+            <ExpenseItem
+              key={expenses.id}
+              title={expenses.title}
+              amount={expenses.amount}
+              date={expenses.date}
+            ></ExpenseItem>
+          ))
+        )} */}
+        {/* {filteredExpenses.length === 0 && }
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map(expenses => (
+            <ExpenseItem
+              key={expenses.id}
+              title={expenses.title}  
+              amount={expenses.amount}
+              date={expenses.date}
+            ></ExpenseItem>
+          ))} */}
+
+        <ExpensesList items={filteredExpenses}></ExpensesList>
+      </Card>
+    </>
+  );
+};
+
+export default Expenses;
